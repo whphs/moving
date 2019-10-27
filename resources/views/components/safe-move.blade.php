9287bc17fb4f1ae11d1693e1c5da118e0d57f141
Xiao Zheng <star97529@hotmail.com>
2019-10-28 03:39:46 +0800

    aaa

--- resources/views/components/safe-move.blade.php
+++ resources/views/components/safe-move.blade.php
@@ -40,7 +40,7 @@
         $.ajax({
             type: 'POST',
             url: '/put_session',
-            data: {key: 'scale_id', value: scaleId}
+            data: {scale_id: scaleId}
         });
 
         window.location.href = "safe_move/detail";
